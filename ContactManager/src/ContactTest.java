c8e32d72e8874c8db4917c69b8034e66ffe4953d
BasilMason <basilmason@hotmail.com>
2015-02-15 16:38:36 +0000

    ContactTest testGetId assertEquals test added.

--- ContactManager/src/ContactTest.java
+++ ContactManager/src/ContactTest.java
@@ -1,12 +1,16 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 
 public class ContactTest {
 
+    private Contact c;
+
     @Before
     public void setUp() throws Exception {
+
+        c = new ContactImpl(1, "Basil Mason");
 
     }
 
@@ -17,6 +21,8 @@ public class ContactTest {
 
     @Test
     public void testGetId() throws Exception {
+
+        assertEquals(1, c.getId());
 
     }
 
