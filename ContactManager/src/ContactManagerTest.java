0ab50be3452d0345ac7d8780d957271cb0a9e772
BasilMason <basilmason@hotmail.com>
2015-02-15 16:44:44 +0000

    ContactManagerTest setup continues. Imports included.

--- ContactManager/src/ContactManagerTest.java
+++ ContactManager/src/ContactManagerTest.java
@@ -1,81 +1,80 @@
-import static org.junit.Assert.*;
-import static org.junit.After.*;
-import static org.junit.Before.*;
-import static org.junit.Test.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ContactManagerTest {
 
-    @org.junit.Before
+    @Before
     public void setUp() throws Exception {
 
     }
 
-    @org.junit.After
+    @After
     public void tearDown() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testAddFutureMeeting() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetPastMeeting() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetFutureMeeting() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetMeeting() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetFutureMeetingList() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetFutureMeetingList1() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetPastMeetingList() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testAddNewPastMeeting() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testAddMeetingNotes() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testAddNewContact() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetContacts() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetContacts1() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testFlush() throws Exception {
 
     }
