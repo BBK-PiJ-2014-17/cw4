d69368648a22163d4b6a0879cdb6f0a429bd6566
BasilMason <basilmason@hotmail.com>
2015-02-15 17:55:54 +0000

    MeetingImpl created.

---
+++ ContactManager/src/MeetingImpl.java
@@ -0,0 +1,27 @@
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * Created by Basil on 15/02/2015.
+ *
+ * An implementation of the Meeting interface
+ */
+
+public class MeetingImpl implements Meeting {
+
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public Calendar getDate() {
+        return null;
+    }
+
+    @Override
+    public Set<Contact> getContacts() {
+        return null;
+    }
+}
