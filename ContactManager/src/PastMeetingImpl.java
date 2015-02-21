0bb3faf5edc024ee35bb74737d3cf3760d00afb7
BasilMason <basilmason@hotmail.com>
2015-02-21 11:36:31 +0000

    PastMeetingImpl created

---
+++ ContactManager/src/PastMeetingImpl.java
@@ -0,0 +1,36 @@
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * Created by Basil on 21/02/2015.
+ *
+ *  An implementation of the Past Meeting interface
+ */
+public class PastMeetingImpl implements PastMeeting {
+
+
+    public PastMeetingImpl() {
+
+
+    }
+
+    @Override
+    public String getNotes() {
+        return null;
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public Calendar getDate() {
+        return null;
+    }
+
+    @Override
+    public Set<Contact> getContacts() {
+        return null;
+    }
+}
