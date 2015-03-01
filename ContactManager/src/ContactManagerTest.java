b587184dfca7a346802d063eadf5d742fd5a979f
BasilMason <basilmason@hotmail.com>
2015-03-01 13:09:29 +0000

    ContactManager tests started.
    AddFutureMeeting test added and exception handling test for meeting set in past.
    
    Validation for contacts to be added.

--- ContactManager/src/ContactManagerTest.java
+++ ContactManager/src/ContactManagerTest.java
@@ -1,15 +1,29 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContactManagerTest {
 
-    private ContactManager cm;
+    private ContactManager contactManager;
+    private Set<Contact> contacts;
 
     @Before
     public void setUp() throws Exception {
 
-        cm = new ContactManagerImpl();
+        // general contacts set
+        contacts = new HashSet<Contact>();
+        contacts.add(new ContactImpl(1, "Basil Mason"));
+        contacts.add(new ContactImpl(2, "Rebecca White"));
+
+        // contact manager
+        contactManager = new ContactManagerImpl();
 
     }
 
@@ -18,10 +32,64 @@ public class ContactManagerTest {
 
     }
 
+    // Rule for exception testing
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    // addFutureMeeting Tests * 3
+
+    /**
+     * Test addFutureMeeting method of ContactManager
+     * Check unique ID returned
+     */
     @Test
     public void testAddFutureMeeting() throws Exception {
 
+        Calendar future = Calendar.getInstance();
+        future.add(Calendar.DAY_OF_MONTH, +1);   // future meeting
+        //FutureMeeting futureMeeting = new FutureMeetingImpl(future, contacts);
+        int m1 = contactManager.addFutureMeeting(contacts, future);
+
+        future.add(Calendar.DAY_OF_MONTH, +1);   // change meeting date
+        int m2 = contactManager.addFutureMeeting(contacts, future);
+
+        System.out.println("Meeting 1 ID: " + m1);
+        System.out.println("Meeting 2 ID: " + m2);
+        assertTrue(m1 != m2);
+
     }
+
+    /**
+     * Test addFutureMeeting method of ContactManager
+     * Check IllegalArgumentException thrown if the meeting is set for a time in the past
+     */
+    @Test
+    public void testAddFutureMeetingThrowsExceptionIfPastMeeting() {
+
+        Calendar past = Calendar.getInstance();
+        past.add(Calendar.DAY_OF_MONTH, -1);   // past meeting
+        //FutureMeeting pastMeeting = new FutureMeetingImpl(past,contacts);
+
+        thrown.expect(IllegalArgumentException.class);
+        contactManager.addFutureMeeting(contacts, past);
+
+    }
+
+    /**
+     * Test addFutureMeeting method of ContactManager
+     * Check IllegalArgumentException thrown if invalid contacts
+     */
+    @Test
+    public void testAddFutureMeetingThrowsExceptionInvalidContacts() {
+
+        Calendar past = Calendar.getInstance();
+        past.add(Calendar.DAY_OF_MONTH, -1);   // past meeting
+        FutureMeeting pastMeeting = new FutureMeetingImpl(past,contacts);
+
+
+    }
+
+    // getPastMeeting
 
     @Test
     public void testGetPastMeeting() throws Exception {
